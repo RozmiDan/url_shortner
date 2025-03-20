4417317e76cee76c62313cfa60c77d380aeab62c
Daniel <rozmirski@mail.ru>
2025-03-20 13:37:48 +0300

    WIP

--- internal/http-server/handlers/update/update.go
+++ internal/http-server/handlers/update/update.go
@@ -1,20 +1,59 @@
 package update_handler
 
-import "log/slog"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
+)
 
 type URLUpdater interface {
 	UpdateURL(currAlias string, newAlias string) error
 }
 
 type Request struct {
-	currAlias string	`json:"url" validate:"required,url"`
-	newAlias string		`json:"url" validate:"required,url"`
+	NewAlias string `json:"newAlias"`
 }
 
 type Response struct {
-
+	Status string `json:"status"`
+	Error  string `json:"error,omitempty"`
+	Alias  string `json:"alias"`
 }
 
-func NewUpdateHandler(logger *slog.Logger, storage URLUpdater) {
+func NewUpdateHandler(logger *slog.Logger, storage URLUpdater) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.update.newupdatehandler"
 
+		logger = logger.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req Request
+
+		reqAlias := chi.URLParam(r, "alias")
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			logger.Error("failed to decode request body")
+			render.JSON(w, r, Response{
+				Status: "Error",
+				Error:  "failed to decode request",
+			})
+			return
+		}
+
+		logger.Info("request body decoded", slog.Any("request", req))
+
+		if req.NewAlias == "" {
+			alias = random.NewAliasForURL(aliasLength)
+		}
+
+
+
+
+	}
 }
